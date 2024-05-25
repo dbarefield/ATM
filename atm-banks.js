9be1865123a0152725a300cd934990cc28ae60d5
dbarefield <136929698+dbarefield@users.noreply.github.com>
2024-05-25 00:13:47 -0400

    Add files via upload

---
+++ atm-banks.js
@@ -0,0 +1,378 @@
+loadBanks([
+  {
+    "State": "Arizona",
+    "Bank Name": "Bank of America"
+  },
+  {
+    "State": "Arizona",
+    "Bank Name": "Republic Bank AZ"
+  },
+  {
+    "State": "Arkansas",
+    "Bank Name": "Reliance Bank"
+  },
+  {
+    "State": "California",
+    "Bank Name": "Bay Federal Credit Union"
+  },
+  {
+    "State": "California",
+    "Bank Name": "Comerica Bank"
+  },
+  {
+    "State": "California",
+    "Bank Name": "JP Morgan Chase"
+  },
+  {
+    "State": "California",
+    "Bank Name": "Mission Bank"
+  },
+  {
+    "State": "California",
+    "Bank Name": "US Bank"
+  },
+  {
+    "State": "California",
+    "Bank Name": "Wells Fargo Bank"
+  },
+  {
+    "State": "Colorado",
+    "Bank Name": "Alpine Bank"
+  },
+  {
+    "State": "Connecticut",
+    "Bank Name": "Citizens Bank"
+  },
+  {
+    "State": "Connecticut",
+    "Bank Name": "JPMorgan Chase"
+  },
+  {
+    "State": "Connecticut",
+    "Bank Name": "Webster Bank NA"
+  },
+  {
+    "State": "Florida",
+    "Bank Name": "Branch Banking and Trust Co"
+  },
+  {
+    "State": "Florida",
+    "Bank Name": "By star Credit Union"
+  },
+  {
+    "State": "Florida",
+    "Bank Name": "GTE Federal Credit Union"
+  },
+  {
+    "State": "Florida",
+    "Bank Name": "People's Alliance FCU"
+  },
+  {
+    "State": "Florida",
+    "Bank Name": "PNC Bank"
+  },
+  {
+    "State": "Florida",
+    "Bank Name": "Regions Bank"
+  },
+  {
+    "State": "Georgia",
+    "Bank Name": "PNC Bank"
+  },
+  {
+    "State": "Georgia",
+    "Bank Name": "Regions Bank"
+  },
+  {
+    "State": "Georgia",
+    "Bank Name": "Truist Bank"
+  },
+  {
+    "State": "Hawaii",
+    "Bank Name": "First Hawaiian Bank"
+  },
+  {
+    "State": "Idaho",
+    "Bank Name": "Idaho Central Credit Union"
+  },
+  {
+    "State": "Illinois",
+    "Bank Name": "1st National Bank of Ottawa"
+  },
+  {
+    "State": "Illinois",
+    "Bank Name": "Associated Bank"
+  },
+  {
+    "State": "Illinois",
+    "Bank Name": "Bank of America"
+  },
+  {
+    "State": "Illinois",
+    "Bank Name": "Beverly Bank and Trust"
+  },
+  {
+    "State": "Illinois",
+    "Bank Name": "BMO Harris Bank"
+  },
+  {
+    "State": "Illinois",
+    "Bank Name": "Byline Bank"
+  },
+  {
+    "State": "Illinois",
+    "Bank Name": "Heartland Bank and Trust Co"
+  },
+  {
+    "State": "Illinois",
+    "Bank Name": "Hindsdale Bank and Trust Company"
+  },
+  {
+    "State": "Illinois",
+    "Bank Name": "Holcomb State Bank"
+  },
+  {
+    "State": "Illinois",
+    "Bank Name": "Illinois Bank and Trust"
+  },
+  {
+    "State": "Illinois",
+    "Bank Name": "JPMorgan Chase"
+  },
+  {
+    "State": "Illinois",
+    "Bank Name": "Morton Community Bank"
+  },
+  {
+    "State": "Illinois",
+    "Bank Name": "Northbrook Bank and Trust"
+  },
+  {
+    "State": "Illinois",
+    "Bank Name": "Peoples National Bank"
+  },
+  {
+    "State": "Illinois",
+    "Bank Name": "PNC Bank"
+  },
+  {
+    "State": "Illinois",
+    "Bank Name": "TD Bank"
+  },
+  {
+    "State": "Illinois",
+    "Bank Name": "US Bank"
+  },
+  {
+    "State": "Indiana",
+    "Bank Name": "Interracial Credit Union"
+  },
+  {
+    "State": "Iowa",
+    "Bank Name": "I H Mississippi Valley Credit Union"
+  },
+  {
+    "State": "Kansas",
+    "Bank Name": "Mainstreet Federal Credit Union"
+  },
+  {
+    "State": "Maryland",
+    "Bank Name": "Citibank"
+  },
+  {
+    "State": "Maryland",
+    "Bank Name": "Hebron Savings Bank"
+  },
+  {
+    "State": "Maryland",
+    "Bank Name": "Shore United Bank"
+  },
+  {
+    "State": "Massachusetts",
+    "Bank Name": "St Anne's Credit Union"
+  },
+  {
+    "State": "Michigan",
+    "Bank Name": "4Front Credit Union"
+  },
+  {
+    "State": "Michigan",
+    "Bank Name": "Comerica Bank"
+  },
+  {
+    "State": "Michigan",
+    "Bank Name": "Fifth Third Bank"
+  },
+  {
+    "State": "Michigan",
+    "Bank Name": "Frankenmuth Credit Union"
+  },
+  {
+    "State": "Michigan",
+    "Bank Name": "PNC Bank"
+  },
+  {
+    "State": "Nevada",
+    "Bank Name": "US Bank"
+  },
+  {
+    "State": "New Jersey",
+    "Bank Name": "Key Bank"
+  },
+  {
+    "State": "New Jersey",
+    "Bank Name": "Valley National Bank"
+  },
+  {
+    "State": "New Mexico",
+    "Bank Name": "BOKF, NA"
+  },
+  {
+    "State": "New York",
+    "Bank Name": "Capital One"
+  },
+  {
+    "State": "New York",
+    "Bank Name": "ESL Federal Credit Union"
+  },
+  {
+    "State": "New York",
+    "Bank Name": "Family First of NY FCU"
+  },
+  {
+    "State": "New York",
+    "Bank Name": "Key Bank"
+  },
+  {
+    "State": "New York",
+    "Bank Name": "PONCE DE LEON FEDERAL BANK"
+  },
+  {
+    "State": "New York",
+    "Bank Name": "State Employees Federal Credit Union"
+  },
+  {
+    "State": "New York",
+    "Bank Name": "TD Bank"
+  },
+  {
+    "State": "North Carolina",
+    "Bank Name": "First Citizens Bank and Trust"
+  },
+  {
+    "State": "North Carolina",
+    "Bank Name": "Truliant Federal Credit Union"
+  },
+  {
+    "State": "North Carolina",
+    "Bank Name": "United Bank"
+  },
+  {
+    "State": "Ohio",
+    "Bank Name": "Key Bank"
+  },
+  {
+    "State": "Ohio",
+    "Bank Name": "PNC Bank"
+  },
+  {
+    "State": "Ohio",
+    "Bank Name": "Wesbanco Bank"
+  },
+  {
+    "State": "Oklahoma",
+    "Bank Name": "First Enterprise Bank"
+  },
+  {
+    "State": "Oklahoma",
+    "Bank Name": "Interbank"
+  },
+  {
+    "State": "Oklahoma",
+    "Bank Name": "PNC Bank"
+  },
+  {
+    "State": "South Dakota",
+    "Bank Name": "First Dakota National Bank"
+  },
+  {
+    "State": "Texas",
+    "Bank Name": "Branch Banking and Trust"
+  },
+  {
+    "State": "Texas",
+    "Bank Name": "Comerica Bank"
+  },
+  {
+    "State": "Texas",
+    "Bank Name": "Frost Bank"
+  },
+  {
+    "State": "Texas",
+    "Bank Name": "Midland States Bank"
+  },
+  {
+    "State": "Texas",
+    "Bank Name": "My Community Credit Union"
+  },
+  {
+    "State": "Texas",
+    "Bank Name": "Niches Federal Credit Union"
+  },
+  {
+    "State": "Texas",
+    "Bank Name": "PNC Bank"
+  },
+  {
+    "State": "Texas",
+    "Bank Name": "Regions Bank"
+  },
+  {
+    "State": "Texas",
+    "Bank Name": "Security Service FCU"
+  },
+  {
+    "State": "Texas",
+    "Bank Name": "Southwest Heritage Credit Union"
+  },
+  {
+    "State": "Texas",
+    "Bank Name": "West star Bank"
+  },
+  {
+    "State": "Texas",
+    "Bank Name": "Western Bank"
+  },
+  {
+    "State": "Utah",
+    "Bank Name": "Bank of Utah"
+  },
+  {
+    "State": "Virginia",
+    "Bank Name": "Branch Banking and Trust Company"
+  },
+  {
+    "State": "Virginia",
+    "Bank Name": "Union Bank and Trust"
+  },
+  {
+    "State": "Virginia",
+    "Bank Name": "Wells Fargo Bank"
+  },
+  {
+    "State": "Wisconsin",
+    "Bank Name": "Associated Bank"
+  },
+  {
+    "State": "Wisconsin",
+    "Bank Name": "Bank First National"
+  },
+  {
+    "State": "Wisconsin",
+    "Bank Name": "CO Vantage Credit Union"
+  },
+  {
+    "State": "Wisconsin",
+    "Bank Name": "Ionia Bank"
+  }
+]);
